6f30077a409edda0c5e957a2bee12bd539acd763
AlexPires1 <alexpirespereira747@gmail.com>
2026-05-15 22:15:33 -0300

    Corrige formatação do rds.tf

--- rds.tf
+++ rds.tf
@@ -1,14 +1,14 @@
 resource "aws_db_instance" "postgres_db" {
-  identifier           = "diagram-db-instance"
-  allocated_storage    = 20
-  engine               = "postgres"
-  engine_version       = "15"
-  instance_class       = "db.t3.micro"
-  db_name              = "diagram_db"
-  username             = var.db_username
-  password             = var.db_password
-  skip_final_snapshot  = true
-  publicly_accessible  = true
+  identifier             = "diagram-db-instance"
+  allocated_storage      = 20
+  engine                 = "postgres"
+  engine_version         = "15"
+  instance_class         = "db.t3.micro"
+  db_name                = "diagram_db"
+  username               = var.db_username
+  password               = var.db_password
+  skip_final_snapshot    = true
+  publicly_accessible    = true
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
 }
 
